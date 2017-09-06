9f0042420bd7466865ee7f0f8f76a135a3686465
FrancoBobadilla <1709673@ucc.edu.ar>
2017-09-06 11:33:06 -0300

    Clase lista terminada

--- Lista/Lista.h
+++ Lista/Lista.h
@@ -3,19 +3,14 @@
 
 #include "Nodo.h"
 
-/**
- * Clase que implementa una Lista Enlasada generica, ya que puede
- * almacenar cualquier tipo de dato T
- * @tparam T cualquier tipo de dato
- */
 template<class T>
 class Lista {
 private:
-    Nodo<T> inicio;
+    Nodo<T>* inicio;
 public:
     Lista();
 
-    Lista(const Lista<T> &li);
+    Lista(Lista<T> &li);
 
     ~Lista();
 
@@ -39,162 +34,150 @@ public:
 };
 
 
-/**
- * Constructor de la clase Lista
- * @tparam T
- */
 template<class T>
 Lista<T>::Lista() {
-  inicio=NULL;
+    inicio = NULL;
 }
 
 
-/**
- * Constructor por copia de la clase Lista
- * @tparam T
- * @param li
- */
 template<class T>
-Lista<T>::Lista(const Lista<T> &li) {}
-
-
-/**
- * Destructor de la clase Lista, se encarga de liberar la memoria de todos los nodos
- * utilizados en la lista
- * @tparam T
- */
-template<class T>
-Lista<T>::~Lista() {}
-
-
-/**
- * Metodo para saber si la lista esta vacia
- * @tparam T
- * @return true si la lista esta vacia, sino false
- */
-template<class T>
-bool Lista<T>::esVacia(){
-  return inicio==NULL;
+Lista<T>::Lista(Lista<T> &li) {
+    inicio = NULL;
+    int fin = li.getTamanio();
+    for (int i=0 ; i<fin ; i++)
+        this->insertar(i, li.getDato(i));
 }
 
 
-/**
- * Metodo para obtener la cantidad de nodos de la lista
- * @tparam T
- * @return la cantidad de nodos de la lista
- */
+template<class T>
+Lista<T>::~Lista() {
+    this->vaciar();
+}
+
+
+template<class T>
+bool Lista<T>::esVacia() {
+        return inicio == NULL;
+}
+
+
 template<class T>
 int Lista<T>::getTamanio() {
-  Nodo<T> *aux=inicio;
-  int cont=0;
-  while(aux!=NULL){
-    cont++;
-    aux=aux->getNext();
-  }
-  return cont;
+    Nodo<T> *aux = inicio;
+    int cont = 0;
+    while (aux != NULL) {
+        cont++;
+        aux = aux->getNext();
+    }
+    return cont;
 }
 
 
-/**
- * Inserta un nodo con el dato en la posicion pos
- * @tparam T
- * @param pos lugar donde será insertado el dato
- * @param dato  dato a insertar
- */
 template<class T>
 void Lista<T>::insertar(int pos, T dato) {
-  
-  if(pos == 0){
-    Nodo<T> *nn = new Nodo<T>(dato,inicio);
-    inicio=nn;
-    return;
-  }
-  
-   int cont=0;
-   Nodo<T> * aux=inicio;
-   
-   while(aux!=NULL && cont<pos-1){
-     cont++;
-     aux = aux->getNext();
-   }
-   
-   if(aux == NULL){
-     throw 1;
-     return;
-   }
-   
-   Nodo<T> *nn = new Nodo<T>(dato,aux->getNext());
-   aux->setNext(nn);
+    if (pos == 0) {
+        Nodo<T> *nn = new Nodo<T>(dato, inicio);
+        inicio = nn;
+        return;
+    }
+    int cont = 0;
+    Nodo<T> *aux = inicio;
+
+    while (aux != NULL && cont < pos - 1) {
+        cont++;
+        aux = aux->getNext();
+    }
+    if (aux == NULL) throw 1;
+
+    Nodo<T> *nn = new Nodo<T>(dato, aux->getNext());
+    aux->setNext(nn);
 }
 
 
-/**
- * Inserta un nodo con el dato en la primera posicion
- * @tparam T
- * @param dato dato a insertar
- */
 template<class T>
-void Lista<T>::insertarPrimero(T dato){
-  Nodo<T> *nn = new Nodo<T>(dato,inicio);
-  inicio=nn;
+void Lista<T>::insertarPrimero(T dato) {
+    Nodo<T> *nn = new Nodo<T>(dato, inicio);
+    inicio = nn;
 }
 
 
-/**
- * Inserta un nodo con el dato en la ultima posicion
- * @tparam T
- * @param dato dato a insertar
- */
 template<class T>
-void Lista<T>::insertarUltimo(T dato){
-  Nodo<T> *aux=inicio;
-  int cont=0;
-  while(aux!=NULL){
-    cont++;
-    aux=aux->getNext();
-  }
-  Nodo<T> *nn = new Nodo<T>(dato,aux->getNext());
-  aux->setNext(nn);
+void Lista<T>::insertarUltimo(T dato) {
+    Nodo<T>* aux = inicio;
+    int cont = 0;
+    if(inicio == NULL)Lista<T>::insertarPrimero( dato);
+    else {
+        while (aux->getNext() != NULL) {
+            cont++;
+            aux = aux->getNext();
+        }
+        Nodo<T> *nn = new Nodo<T>(dato, NULL);
+        aux->setNext(nn);
+    }
 }
 
 
-/**
- * Elimina el nodo en la posicion 'pos' de la lista enlasada
- * @tparam T
- * @param pos posicion del nodo a eliminar
- */
 template<class T>
 void Lista<T>::remover(int pos) {
-  
+    if (pos == 0) {
+        inicio = NULL;
+        return;
+    }
+
+    int cont = 0;
+    Nodo<T> *aux = inicio;
+    Nodo<T> *tmp;
+
+    while (aux != NULL && cont < pos-1) {
+        cont++;
+        aux = aux->getNext();
+    }
+
+    if(aux==NULL) throw 1;
+
+    tmp = aux->getNext();
+    aux->setNext(tmp->getNext());
+    delete tmp;
 }
 
 
-/**
- * Obtener el dato del nodo en la posicion pos
- * @tparam T
- * @param pos posicion del dato
- * @return dato almacenado en el nodo
- */
 template<class T>
-T Lista<T>::getDato(int pos) {}
+T Lista<T>::getDato(int pos) {
+    Nodo<T> *aux=inicio;
+    int cont=0;
+    while(cont<pos && aux != NULL) {
+        cont++;
+        aux = aux->getNext();
+    }
+    if(pos != cont) throw 1;
+    return aux->getDato();
+}
 
 
-/**
- * Reemplaza el dato almacenado en un nodo por este otro
- * @tparam T
- * @param pos posicion donde se desea reemplazar
- * @param dato nuevo dato a almacenar
- */
 template<class T>
-void Lista<T>::reemplazar(int pos, T dato) {}
+void Lista<T>::reemplazar(int pos, T dato) {
+    Nodo<T> *aux=inicio;
+    int cont=0;
+    while(cont<pos && aux != NULL) {
+        cont++;
+        aux = aux->getNext();
+    }
+    if (aux == NULL)throw 1;
+    aux->setDato(dato);
+}
 
 
-/**
- * Función que vacia la lista enlazada
- * @tparam T
- */
 template<class T>
-void Lista<T>::vaciar() {}
+void Lista<T>::vaciar() {
+    Nodo<T> *aux=inicio;
+    Nodo<T> *ant;
+    while(aux != NULL) {
+        ant = aux;
+        aux = aux->getNext();
+        delete ant;
+    }
+    inicio=NULL;
+}
 
 
 #endif //LISTA_H
